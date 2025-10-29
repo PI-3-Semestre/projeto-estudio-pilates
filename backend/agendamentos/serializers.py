fe644f57e95197b2ce1c56969bf3eaa53d7a7dae
DaviBMachado <davimachado91210@gmail.com>
2025-10-28 23:13:04 -0300

    Refatora serializers de Aula, Modalidade e Bloqueio

--- backend/agendamentos/serializers.py
+++ backend/agendamentos/serializers.py
@@ -10,21 +10,6 @@ class HorarioTrabalhoSerializer(serializers.ModelSerializer):
         model = HorarioTrabalho
         fields = '__all__'
 
-class BloqueioAgendaSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = BloqueioAgenda
-        fields = '__all__'
-
-class ModalidadeSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Modalidade
-        fields = '__all__'
-
-class AulaSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Aula
-        fields = '__all__'
-
 class AulaAlunoSerializer(serializers.ModelSerializer):
     class Meta:
         model = AulaAluno
@@ -39,3 +24,48 @@ class ListaEsperaSerializer(serializers.ModelSerializer):
     class Meta:
         model = ListaEspera
         fields = '__all__'
+
+class ModalidadeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Modalidade
+        fields = ['id', 'nome']
+
+class BloqueioAgendaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BloqueioAgenda
+        fields = ['id', 'data', 'descricao', 'studio']
+        
+class AulaWtriteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Aula
+        fields = [
+            "data_hora_inicio",
+            "duracao_minutos",
+            "capacidade_maxima",
+            "tipo_aula",
+            "modalidade",
+            "studio",
+            "instrutor_principal",
+            "instrutor_substituto",
+        ]
+
+class AulaReadSerializer(serializers.ModelSerializer):
+    modalidade = ModalidadeSerializer(read_only=True)
+    instructor_principal=serializers.StringRelatetField(read_only=True)
+    instructor_substituto=serializers.StringRelatedField(read_only=True)
+    studio = serializers.StringRelatedField(read_only=True)
+    
+    class Meta:
+        model = Aula
+        field = [
+            "id",
+            "data_hora_inicio",
+            "duracao_minutos",
+            "capacidade_maxima",
+            "tipo_aula",
+            "modalidade",
+            "studio",
+            "instrutor_principal",
+            "instrutor_substituto",
+        ]
+    
