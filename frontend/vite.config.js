b881b3bb378bdde1b2eba0771599ff1d21e18f87
DynamicBR <dmitrijnferreira@gmail.com>
2025-11-29 11:59:10 -0300

    Forçar deploy na vercel

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+/*Tentando dar deploy na Vercel */
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,7 +9,7 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: path.resolve(__dirname, 'index.html'),
+        main: path.resolve(__dirname, "index.html"),
       },
     },
   },
