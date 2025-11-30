86fa5bac668de00b6587874d8ec494c524a61739
Dmitri José <dmitrijnferreira@gmail.com>
2025-11-29 22:32:28 -0300

    Update Vite config to enforce current directory as root 3

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -14,13 +14,14 @@ export default defineConfig({
     },
   },
 });*/
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: "./",  // <--- Adicionamos isso para forçar a raiz ser a pasta atual
+  root: "./",  // <--- ISSO OBRIGA O VITE A OLHAR PARA O PRÓPRIO PÉ
   build: {
     outDir: "dist",
   }
